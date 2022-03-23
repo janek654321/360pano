67cf7cff63e48d348e2d5a8703e5783de6bbccd2
jw <janwojcik2004@gmail.com>
2022-03-23 23:02:41 +0100

    XLO

---
+++ data.js
@@ -0,0 +1,122 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-xlo",
+      "name": "XLO",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.1706463258492974,
+        "pitch": 0.22260216227637386,
+        "fov": 1.3793455132008117
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.4125619808453518,
+          "pitch": 0.34989910924555545,
+          "rotation": 12.566370614359176,
+          "target": "0-xlo"
+        },
+        {
+          "yaw": -0.7473800098793824,
+          "pitch": 0.42818979682988356,
+          "rotation": 6.283185307179586,
+          "target": "0-xlo"
+        },
+        {
+          "yaw": -2.900411322709031,
+          "pitch": 0.35688629668114835,
+          "rotation": 0,
+          "target": "0-xlo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.0748646486657343,
+          "pitch": 0.7030187914337382,
+          "title": "Boisko do koszykówki",
+          "text": ""
+        },
+        {
+          "yaw": -2.1968667286740384,
+          "pitch": 0.7500556934512446,
+          "title": "Boisko do piłki nożnej",
+          "text": ""
+        },
+        {
+          "yaw": 2.0996450747409607,
+          "pitch": 0.4415881695518795,
+          "title": "Boisko do siatkówki plażowej",
+          "text": ""
+        },
+        {
+          "yaw": -3.1217728303716665,
+          "pitch": 0.508975552463788,
+          "title": "Bieżnia",
+          "text": ""
+        },
+        {
+          "yaw": 1.44150303385244,
+          "pitch": 0.03392430316351458,
+          "title": "Budynek B",
+          "text": ""
+        },
+        {
+          "yaw": -2.9000023216883797,
+          "pitch": 0.08371235488927553,
+          "title": "Hala sportowa<div><br></div>",
+          "text": "<div style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">- siłownia<br></div><div style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">- sala judo</div>"
+        },
+        {
+          "yaw": -0.7570145768537593,
+          "pitch": 0.09111730475726354,
+          "title": "Budynek A",
+          "text": ""
+        },
+        {
+          "yaw": -1.885471507803775,
+          "pitch": 0.04102159908250158,
+          "title": "Internat 1",
+          "text": ""
+        },
+        {
+          "yaw": 0.3671181156224197,
+          "pitch": 0.40361167432945777,
+          "title": "Restauracja Róża Wiatrów",
+          "text": ""
+        },
+        {
+          "yaw": 2.207608750672721,
+          "pitch": 0.0088555162132824,
+          "title": "Internat 2",
+          "text": ""
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
