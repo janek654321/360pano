03a280c580c179201cb05495804d15255edc4cbf
jw <janwojcik2004@gmail.com>
2022-03-24 19:32:24 +0100

    V2

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-xlo",
-      "name": "XLO",
+      "id": "0-ptak",
+      "name": "ptak",
       "levels": [
         {
           "tileSize": 256,
@@ -24,99 +24,64 @@ var APP_DATA = {
       ],
       "faceSize": 2048,
       "initialViewParameters": {
-        "yaw": -0.1706463258492974,
-        "pitch": 0.22260216227637386,
+        "yaw": -0.20252725194576726,
+        "pitch": 0.3169113094443432,
         "fov": 1.3793455132008117
       },
       "linkHotspots": [
         {
-          "yaw": 1.4125619808453518,
-          "pitch": 0.34989910924555545,
-          "rotation": 12.566370614359176,
-          "target": "0-xlo"
-        },
-        {
-          "yaw": -0.7473800098793824,
-          "pitch": 0.42818979682988356,
-          "rotation": 6.283185307179586,
-          "target": "0-xlo"
-        },
-        {
-          "yaw": -2.900411322709031,
-          "pitch": 0.35688629668114835,
+          "yaw": -0.7530694490819414,
+          "pitch": 0.3992879063808168,
           "rotation": 0,
-          "target": "0-xlo"
+          "target": "1-partera"
         }
       ],
-      "infoHotspots": [
+      "infoHotspots": []
+    },
+    {
+      "id": "1-partera",
+      "name": "parterA",
+      "levels": [
         {
-          "yaw": -1.0748646486657343,
-          "pitch": 0.7030187914337382,
-          "title": "Boisko do koszykówki",
-          "text": ""
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
         },
         {
-          "yaw": -2.1968667286740384,
-          "pitch": 0.7500556934512446,
-          "title": "Boisko do piłki nożnej",
-          "text": ""
+          "tileSize": 512,
+          "size": 512
         },
         {
-          "yaw": 2.0996450747409607,
-          "pitch": 0.4415881695518795,
-          "title": "Boisko do siatkówki plażowej",
-          "text": ""
+          "tileSize": 512,
+          "size": 1024
         },
         {
-          "yaw": -3.1217728303716665,
-          "pitch": 0.508975552463788,
-          "title": "Bieżnia",
-          "text": ""
-        },
-        {
-          "yaw": 1.44150303385244,
-          "pitch": 0.03392430316351458,
-          "title": "Budynek B",
-          "text": ""
-        },
-        {
-          "yaw": -2.9000023216883797,
-          "pitch": 0.08371235488927553,
-          "title": "Hala sportowa<div><br></div>",
-          "text": "<div style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">- siłownia<br></div><div style=\"font-size: 16px; background-color: rgba(103, 115, 131, 0.8);\">- sala judo</div>"
-        },
-        {
-          "yaw": -0.7570145768537593,
-          "pitch": 0.09111730475726354,
-          "title": "Budynek A",
-          "text": ""
-        },
-        {
-          "yaw": -1.885471507803775,
-          "pitch": 0.04102159908250158,
-          "title": "Internat 1",
-          "text": ""
-        },
-        {
-          "yaw": 0.3671181156224197,
-          "pitch": 0.40361167432945777,
-          "title": "Restauracja Róża Wiatrów",
-          "text": ""
-        },
-        {
-          "yaw": 2.207608750672721,
-          "pitch": 0.0088555162132824,
-          "title": "Internat 2",
-          "text": ""
+          "tileSize": 512,
+          "size": 2048
         }
-      ]
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.5859319631695286,
+        "pitch": 0.08374597758718494,
+        "fov": 1.3793455132008117
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.323373995244479,
+          "pitch": 0.07631795658843288,
+          "rotation": 0,
+          "target": "0-ptak"
+        }
+      ],
+      "infoHotspots": []
     }
   ],
-  "name": "Project Title",
+  "name": "Test 360",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": true,
     "fullscreenButton": true,
-    "viewControlButtons": false
+    "viewControlButtons": true
   }
 };
